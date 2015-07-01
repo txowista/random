69a2f99660a7eb1341c7a21735ec797074f8fa1e
Igor Gonzalez <igonzalez@certiorem.com>
2015-07-01 09:29:15 +0200

    Random Version 1

---
+++ random.js
@@ -0,0 +1,8 @@
+//Math.random() devuelve numero aleatorio entre 0 y 1
+ var numero= Math.random();
+
+ if (numero <= 0.5) {
+   console.log('\n' + numero + ' Menor que 0,5 \n');
+ } else {
+   console.log('\n' + numero + ' Mayor que 0,5 \n' );
+ }
